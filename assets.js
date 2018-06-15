5de50547b2d41546404a6ec479846ee9b9fcdb86
Patrick Choi <patrick.choi@nsuslab.com>
2018-06-15 19:07:05 +0900

    get asset information

--- assets.js
+++ assets.js
@@ -4,24 +4,41 @@ StellarSdk.Network.usePublicNetwork();
 
 const server = new StellarSdk.Server('https://horizon.stellar.org');
 
-let assets = server.assets();
+let assets = [
+	{code:'SIX', issuer:'GDMS6EECOH6MBMCP3FYRYEVRBIV3TQGLOFQIPVAITBRJUMTI6V7A2X6Z'},
+	{code:'CNY', issuer:'GAREELUB43IRHWEASCFBLKHURCGMHE5IF6XSE7EXDLACYHGRHM43RFOX'}
+];
 
-assets
-	.forCode('SIX')
-	.forIssuer('GDMS6EECOH6MBMCP3FYRYEVRBIV3TQGLOFQIPVAITBRJUMTI6V7A2X6Z')
-	.call()
-	.then(function (rst) {
-		rst.records.forEach(function (row, i) {
-			console.log(i, row);
+assets.forEach(function (asset, idx) {
+	server.assets()
+		.forCode(asset.code)
+		.forIssuer(asset.issuer)
+		.call()
+		.then(function (rst) {
+			rst.records.forEach(function (row, i) {
+				console.log(i, row);
 
-			// console.log();
-
-			row.toml().then((res) => {
-				console.log(toml.parse(res));
+				row.toml().then((res) => {
+					console.log(toml.parse(res));
+				});
 			});
 		});
-	});
+});
 
+// get asset information
+// server.assets()
+// 	.forCode('SIX')
+// 	.forIssuer('GDMS6EECOH6MBMCP3FYRYEVRBIV3TQGLOFQIPVAITBRJUMTI6V7A2X6Z')
+// 	.call()
+// 	.then(function (rst) {
+// 		rst.records.forEach(function (row, i) {
+// 			console.log(i, row);
+//
+// 			row.toml().then((res) => {
+// 				console.log(toml.parse(res));
+// 			});
+// 		});
+// 	});
 
 // StellarSdk.StellarTomlResolver.resolve('six.network')
 // 	.then(stellarToml => {
