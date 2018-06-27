385b4be22ff3516527a1a0a51c70608f2f79d5bd
harleykwon <harley.kwon@nsuslab.com>
2018-06-27 14:29:05 +0900

    Revert "Merge branch 'master' of https://github.com/ncoin/stellar-sdk-test"
    This reverts commit 4022b3bc8fed2e0e3ebc4c8186684c091a016b50, reversing
    changes made to 6ba16446f76240e40b9eb570e2c19688a766cca7.

--- NCH100_Loan.js
+++ NCH100_Loan.js
@@ -3,28 +3,30 @@ StellarSdk.Network.useTestNetwork();
 
 const server = new StellarSdk.Server('https://horizon-testnet.stellar.org');
 
-const User = "SDA4VNK7G4AFXLERM6KP42WP2FSX3UH7FMBOWWGPGLEPQ4LAKO4UV5UH";
+const Buyer = "SDA4VNK7G4AFXLERM6KP42WP2FSX3UH7FMBOWWGPGLEPQ4LAKO4UV5UH";
 
 const Issuer = "SBCJEQRONAQ533FEAUXSYDB373SN5AQEERWLLQ3PXJE4QYPJGEI73TTD";
+const Seller = "SA6PDGIJELKKDOFLSJRMCGA7KE65A6EHNQZXTJEY5TQTGSUH4O3NK2Q3";
 const Loan = "SC6ZEE6RNDJIBU7KN25GDOWZMENLDFXMO7PYB753SXEWNSO4ENAW7DPB";
 
-const UserKey = StellarSdk.Keypair.fromSecret(User);
+const BuyerKey = StellarSdk.Keypair.fromSecret(Buyer);
 
 const IssuerKey = StellarSdk.Keypair.fromSecret(Issuer);
+const SellerKey = StellarSdk.Keypair.fromSecret(Seller);
 const LoanKey = StellarSdk.Keypair.fromSecret(Loan);
 
 let transaction;
 
 // the JS SDK uses promises for most actions, such as retrieving an account
-server.loadAccount(UserKey.publicKey())
+server.loadAccount(BuyerKey.publicKey())
 	.catch(StellarSdk.NotFoundError, function (error) {
 		throw new Error('The Singer account does not exist!');
 	})
 	// If there was no error, load up-to-date information on your account.
 	.then(function() {
-		return server.loadAccount(UserKey.publicKey());
+		return server.loadAccount(BuyerKey.publicKey());
 	})
-	.then(function(account) {
+	.then(function(BuyerAccount) {
 		let nch100 = new StellarSdk.Asset("NCH100", IssuerKey.publicKey());
 		let amount = 1000;
 		let rate = 0.5;
@@ -33,16 +35,19 @@ server.loadAccount(UserKey.publicKey())
 
 		// console.log(invoice.toFixed(7));
 
-		transaction = new StellarSdk.TransactionBuilder(account)
+		transaction = new StellarSdk.TransactionBuilder(BuyerAccount)
+			// .addOperation(StellarSdk.Operation.changeTrust({
+			// 	asset: nch100
+			// }))
 
-			// transfer XLM to loan account from user account
+			// transfer XLM to loan account
 			.addOperation(StellarSdk.Operation.payment({
 				destination: LoanKey.publicKey(),
 				asset: StellarSdk.Asset.native(),
 				amount: amount.toString()
 			}))
 
-			// transfer NCH100 to loan account from issuer account
+			// transfer NCH100 to seller account from loan account
 			.addOperation(StellarSdk.Operation.payment({
 				destination: LoanKey.publicKey(),
 				asset: nch100,
@@ -50,9 +55,9 @@ server.loadAccount(UserKey.publicKey())
 				source: IssuerKey.publicKey()
 			}))
 
-			// transfer NCH100 to user account from loan account
+			// transfer NCH100 to buyer account from loan account
 			.addOperation(StellarSdk.Operation.payment({
-				destination: UserKey.publicKey(),
+				destination: BuyerKey.publicKey(),
 				asset: nch100,
 				amount: loanAmount.toFixed(7),
 				source: LoanKey.publicKey()
@@ -60,7 +65,7 @@ server.loadAccount(UserKey.publicKey())
 			.build();
 
 		transaction.sign(IssuerKey);
-		transaction.sign(UserKey);
+		transaction.sign(BuyerKey);
 		transaction.sign(LoanKey);
 
 		return server.submitTransaction(transaction);
