ac64d6de21234b604b853cf47505adb073b43590
Patrick Choi <patrick.choi@nsuslab.com>
2018-07-16 20:12:04 +0900

    our horizon server test

--- balance.js
+++ balance.js
@@ -1,40 +1,22 @@
 const StellarSdk = require('stellar-sdk');
-StellarSdk.Network.use('Standalone Network ; February 2017');
+StellarSdk.Network.useTestNetwork();
 
 // HTTP 허용 설정
-StellarSdk.Config.setAllowHttp(true);
-console.log("Is allow http:", StellarSdk.Config.isAllowHttp());
+// StellarSdk.Config.setAllowHttp(true);
+// console.log("Is allow http:", StellarSdk.Config.isAllowHttp());
 
-const server = new StellarSdk.Server('http://localhost:8000');
+const server = new StellarSdk.Server('https://nstellar-test.ncoin.com');
 
-console.log(server);
+const Buyer = "SCX4VXACC7CBIQ23DVRRCDTRLY2QQRWBIOLVZVPXX654COLLI3AQZOMN";    // P6
 
-const pair = StellarSdk.Keypair.random();
-
-console.log("P: ", pair.publicKey());
-console.log("S: ", pair.secret());
-
-const request = require('request');
-request.get({
-	url: 'http://localhost:8000/friendbot',
-	qs: {addr: pair.publicKey()},
-	json: true
-}, function (error, response, body) {
-	if (error || response.statusCode !== 200) {
-		console.error('ERROR!', error || body);
-	}
-	else {
-		console.log('SUCCESS! You have a new account :)\n', body);
-	}
-});
-
+const BuyerKey = StellarSdk.Keypair.fromSecret(Buyer);
 
 // the JS SDK uses promises for most actions, such as retrieving an account
-// server.loadAccount(pair.publicKey()).then(function(account) {
-// 	console.log(account);
-//
-// 	console.log('Balances for account: ' + pair.publicKey());
-// 	account.balances.forEach(function(balance) {
-// 		console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
-// 	});
-// });
+server.loadAccount(BuyerKey.publicKey()).then(function(account) {
+	console.log(account);
+
+	console.log('Balances for account: ' + BuyerKey.publicKey());
+	account.balances.forEach(function(balance) {
+		console.log('Type:', balance.asset_type, ', Balance:', balance.balance);
+	});
+});
